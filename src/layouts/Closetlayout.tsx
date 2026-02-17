ca6a89226f8b423e963e5e6ecfbce14575a3bcbb
YangEunJung <ej_33@naver.com>
2026-02-17 20:30:21 +0900

    fix: 배포시  에러 방지를 위한 tsconfig 설정 재재변경

--- src/layouts/Closetlayout.tsx
+++ src/layouts/Closetlayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import BottomNav from '../components/home/Bottomnav';
+import BottomNav from '../components/Home/Bottomnav';
 
 const ClosetLayout = () => {
   return (
